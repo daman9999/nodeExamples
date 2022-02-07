b5f590d4aaed41f1fb897b96fe61b385bde258ec
daman9999 <damanpreetsingh9699@gmail.com>
2022-02-07 22:04:26 +0530

    handle async Functions

---
+++ HandleAsyncFunctions.js
@@ -0,0 +1,21 @@
+// promises
+
+let a= 20 
+let b=0 
+
+let waitingData = new Promise(
+    (resolve,reject)=>{
+        setTimeout(()=>
+        {
+            resolve(30)
+        },1000)
+    }
+)
+
+
+waitingData.then(
+    (data)=>{
+        b=data;
+        console.log(a+b);
+    }
+)
