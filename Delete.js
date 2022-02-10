5fcb2d8ec44eafcde033942a60c659703e407a6d
daman9999 <damanpreetsingh9699@gmail.com>
2022-02-11 01:57:23 +0530

    basic APi

--- Delete.js
+++
@@ -1,9 +0,0 @@
-const dbConnect = require('./MongoDB')
-
-const deleteData = async()=>{
-    const db = await dbConnect();
-    const res= await db.deleteMany(
-            {brand:"peach"}
-    );
-    console.log("DATa DELETED")
-}
--- index.js
+++
@@ -1,9 +0,0 @@
-// import the database file
-const { insert } = require('./insert');
-const dbConnect = require('./MongoDB');
-
-// making a connection with MONGOdb
-
-
-dbConnect();
-insert();
