e3be4e003593f253a8922a69499ee92645f1433b
daman9999 <damanpreetsingh9699@gmail.com>
2022-02-12 15:00:14 +0530

    upload a file using multer

---
+++ UploadingFile.md
@@ -0,0 +1,8 @@
+install multer=> to upload a file
+
+in post man choose body->form-data
+in thunder client body -> form -> mark checkbox files -> choose file fill field.
+
+
+bina multer de v kr skde upload pr code jyada likhna paina
+node fs.createReadStream()
--- index.js
+++ index.js
@@ -1,35 +1,31 @@
+const { response } = require('express');
 const express = require('express')
-require('./config');
-const Products = require('./Product');
+const multer = require('multer');
 const app = express()
 const port = 3000
 
-
-
-// post put delete nal v data search ho janda
-
-app.use(express.json())
-app.get('/search/:key',async(req,res)=>{
-  console.log(req.params.key)
-  // use regex to search
-  let data = await Products.find(
-    // here we cant use price because it is Number in SCHEMA
-    {
-      
-        "$or":[
-         { "name":{$regex:req.params.key}},
-         { "brand":{$regex:req.params.key}},
-         { "category":{$regex:req.params.key}},
-         
-
-        ]
-      
+// upload is a middle-ware
+const upload = multer({
+  storage:multer.diskStorage({
+    // cb=>callback
+    destination:(req,file,cb)=>{
+      // upload is here folder name in cb
+        cb(null,"upload")
+    },
+    filename:(req,file,cb)=>{
+      // save the uploaded file with filename
+      const d = new Date();
+      cb(null,file.fieldname+"-"+`${d.getDate()}-${d.getMonth()}-${d.getFullYear()}[${d.getHours()}h${d.getMinutes()}m${d.getSeconds()}s]`+".jpg")
     }
-  );
-  res.send(data)
+  })
+  // telling that we are uploading only a single file (field name in thunderclient)
+}).single("user_file")
 
+
+app.post('/upload',upload,async(req,res)=>{
+    res.send("uploading a file")
 })
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
